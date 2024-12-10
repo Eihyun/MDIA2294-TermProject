9b4c19405cdb00762098bb330491fa0b4dff0290
Eihyun <keh7370@gmail.com>
2024-12-09 18:37:41 -0800

    Flavor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// hamburger menu
+
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const menuIcon = document.querySelector('.menu-icon');
@@ -8,3 +10,65 @@ hamburger.addEventListener('click', () => {
     menuIcon.classList.toggle('hide');
     closeIcon.classList.toggle('hide');
 })
+
+// Add flavors list
+// array of flavor names
+const flavors = [
+    "True potential as brilliant",
+    "No traffic",
+    "Sunshine",
+    "Peace",
+    "Free samples",
+    "A mother's love",
+    "Strawberry Vinagrette",
+    "Clothes right out of the dryer",
+    "Wasabi",
+    "Rocky road",
+    "Hot shower",
+    "Persimmon",
+    "Sleeping in on a rainy Sunday",
+    "Unmitigated joy",
+    "Cherry",
+    "Kimchi",
+    "Weekend",
+    "Mars landing",
+    "Empty inbox",
+    "Baby kittens",
+    "Three-day weekend",
+    "Tin roof sundae",
+    "Sriracha",
+    "Unexpected hotel upgrade",
+    "Sweet potato fries",
+    "A clean house",
+    "Pralines and cream",
+    "Haircut compliment"
+];
+
+// function to populate flavors
+function addFlavors() {
+    const flavorContainer = document.querySelector('.home-menu__content-flavors');
+
+    // add each flavor as a div
+    flavors.forEach(flavor => {
+        const flavorDiv = document.createElement('div');
+        flavorDiv.className = 'flavor';
+        flavorDiv.textContent = flavor;
+        flavorContainer.appendChild(flavorDiv);
+    });
+}
+
+// call the function when the document is loaded
+document.addEventListener('DOMContentLoaded', addFlavors);
+
+
+// flavor menu button click animation
+const menuBtn = document.querySelector('.flavor-menu-button');
+
+menuBtn.addEventListener('click', function() {
+    this.classList.add('clicked');
+
+    // remove the clicked class after the animation
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 200);
+});
